f629f88e7277d6dee84b36099f7c9ea62668d26a
Bjarne Lundgren <bjarne@Bjarnes-MBP.lan>
2017-12-01 16:08:59 +0100

    experimenting with BottomNavigationView

--- app/src/main/java/com/example/test42/MainActivity.kt
+++ app/src/main/java/com/example/test42/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.test42
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.internal.NavigationMenuItemView
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.MenuItem
 
 import com.example.test42.data.model.Section
 import com.example.test42.data.remote.ApiUtils
 import com.example.test42.data.remote.SectionService
 import com.example.test42.ui.adapters.SectionsViewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 import java.util.ArrayList
 import java.util.concurrent.CopyOnWriteArrayList
@@ -23,6 +26,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // https://kotlinlang.org/docs/reference/lambdas.html
+        // experimenting with lambdas instead of class with one function in it
+        //navigation.setOnNavigationItemSelectedListener { _ -> false }
+        navigation.setOnNavigationItemSelectedListener { item -> didTap(item) }
+    }
+
+    fun didTap(item: MenuItem): Boolean {
+        return false
     }
 
 }
--- app/src/main/java/com/example/test42/data/remote/ApiUtils.kt
+++ app/src/main/java/com/example/test42/data/remote/ApiUtils.kt
@@ -1,13 +1,9 @@
 package com.example.test42.data.remote
 
-import dagger.Module
-import dagger.Provides
-
 /**
  * Created by bjarne on 24/11/2017.
  */
 
-@Module
 object ApiUtils {
     // this API is for our ( TV 2 Danmark A/S ) news app. This content is copyright TV 2 Danmark A/S
     // you should NOT use this except for experimentation purposes - Also it is in Danish :-)
@@ -15,10 +11,8 @@ object ApiUtils {
     private val BASE_URL = "https://app-backend.api.tv2.dk/"
 
     val sectionService: SectionService
-        @Provides
         get() = RetrofitClient.getClient(BASE_URL).create(SectionService::class.java)
 
     val articleService: ArticleService
-        @Provides
         get() = RetrofitClient.getClient(BASE_URL).create(ArticleService::class.java)
 }
