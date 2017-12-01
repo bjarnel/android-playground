bc024cf74863cb56e924fabed9192ca93a22dbe4
Bjarne Lundgren <bjarne@Bjarnes-MBP.lan>
2017-12-01 17:42:02 +0100

    working on fragment switching

--- app/src/main/java/com/example/test42/MainActivity.kt
+++ app/src/main/java/com/example/test42/MainActivity.kt
@@ -11,6 +11,9 @@ import android.view.MenuItem
 import com.example.test42.data.model.Section
 import com.example.test42.data.remote.ApiUtils
 import com.example.test42.data.remote.SectionService
+import com.example.test42.fragments.NewsFragment
+import com.example.test42.fragments.SportsFragment
+import com.example.test42.fragments.WeatherFragment
 import com.example.test42.ui.adapters.SectionsViewAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,6 +26,8 @@ import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
+    private var currentActiveTabId = R.id.action_news
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,7 +39,52 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun didTap(item: MenuItem): Boolean {
+        // basic "guard"
+        //if(currentActiveTabId == item.itemId) { return false }
+        //currentActiveTabId = item.itemId
+
+        // https://android.jlelse.eu/a-few-ways-to-implement-a-swift-like-guard-in-kotlin-ffd94027864e
+        // check this out!
+        currentActiveTabId = (currentActiveTabId != item.itemId)?.let { item.itemId } ?: return false
+
         // https://developer.android.com/training/basics/fragments/fragment-ui.html
+
+        // in Kotlin we use when instead of switch, and it is more powerful
+        // https://kotlinlang.org/docs/reference/control-flow.html
+        /*
+        when (item.itemId) {
+            R.id.action_news -> print("news")
+            R.id.action_sports -> print("sports")
+            R.id.action_weather -> print("weather")
+        }
+        */
+
+        // assignment from the "result" of the when expression
+        /*
+        val newFragment = when (item.itemId) {
+            R.id.action_news -> NewsFragment()
+            R.id.action_sports -> SportsFragment()
+            else -> WeatherFragment()  // R.id.action_weather
+        }
+
+        fragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, newFragment)
+            .commit()
+*/
+        //NOTE: this causes app to crash with view must not be null if NewsFragment has not finished loading articles (onResponse, trying
+        // to update adapter(s)
+        //NOTE: setOnNavigationItemSelectedListener may not be right, at least it does not actually update the currently active tab..
+
+        // shorter version...
+
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, when (item.itemId) {
+                    R.id.action_news -> NewsFragment()
+                    R.id.action_sports -> SportsFragment()
+                    else -> WeatherFragment()
+                })
+                .commit()
+
         return false
     }
 
